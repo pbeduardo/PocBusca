11e5b69af5a72c596c4e43e0dfc596f61336d51b
Eduardo Pereira Borges <eborges@desenvvar30-lnx.dimed.local>
2020-02-21 08:15:09 -0300

    add component busca-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { ModalComponent } from './modal-itens/modal.component';
 import { PrecoItemComponent } from './preco-item/preco-item.component';
 import { EstoqueItemComponent } from './estoque-item/estoque-item.component';
 import { ItemDetalhesComponent } from './item-detalhes/item-detalhes.component';
+import { BuscaItemComponent } from './busca-item/busca-item.component';
 
 @NgModule({
-  declarations: [AppComponent, ModalComponent, PrecoItemComponent, EstoqueItemComponent, ItemDetalhesComponent],
+  declarations: [AppComponent, ModalComponent, PrecoItemComponent, EstoqueItemComponent, ItemDetalhesComponent, BuscaItemComponent],
   
   imports: [BrowserModule, BrowserAnimationsModule, MaterialModule, HttpClientModule],
   providers: [],
---
+++ src/app/busca-item/busca-item.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit } from '@angular/core';
+import { ItemPreco } from '../preco-item/item-preco.model';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from '../modal-itens/modal.component';
+import { Item } from '../item/item.model';
+import { ItemService } from '../item.service'
+import { forkJoin, Observable } from 'rxjs';
+import { switchMap, map, tap } from 'rxjs/operators';
+import { EstoqueItem } from '../estoque-item/estoque';
+
+@Component({
+  selector: 'app-busca-item',
+  templateUrl: './busca-item.component.html'
+})
+export class BuscaItemComponent {
+
+  itens: Item[];
+  itemPreco: ItemPreco[];
+  estoqueItem: EstoqueItem[];
+
+
+  constructor(public dialog: MatDialog, private itemService: ItemService) { }
+
+  //Pressiona Ok na pesquisa e envia o valor para a função!
+  onKey(value: string) {
+    this.enviarViaService(value);
+  }
+
+  //Pesquisando Item
+  enviarViaService(stringPesquisa: string) {
+    //console.log("STRING RECEBIDA PARA PESQUISA: ", stringPesquisa)
+    if (stringPesquisa != "") {
+
+      this.itemService.procuraItem(stringPesquisa)
+        .pipe(
+          map(itens => this.obterDezPrimeirasPosicoes(itens)),
+          switchMap(itens => this.adicionarEstoqueItens(itens)),
+          switchMap(itens => this.adicionarPrecoItens(itens))
+        )
+        .subscribe(itens => {
+          this.itens = itens
+        }, () => this.itens = [])
+
+    } else {
+      this.itens = [];
+    }
+  }
+  //Filra somente os 10 primeiros resultados
+  private obterDezPrimeirasPosicoes(itens: Item[]): Item[] {
+    return itens.filter((item, index) => index < 10);
+  }
+
+  private adicionarPrecoItens(itens: Item[]): Observable<Item[]> {
+    return forkJoin(
+      //Indo no Service, enviando o Código e pegando o preço.
+      itens.map(item => this.itemService.procuraPrecoItem(item.codigoItem)
+        .pipe(
+          tap(itemPreco => this.adicionarPrecoItem(item, itemPreco)),
+          map(() => item)
+        )
+      )
+    );
+  }
+
+  private adicionarEstoqueItens(itens: Item[]): Observable<Item[]> {
+    return forkJoin(
+      //Indo no Service, enviando o Código e pegando o estoque.
+      itens.map(item => this.itemService.procuraEstoqueItem(item.codigoItem)
+        .pipe(
+          tap(estoqueItem => this.adicionarEstoqueItem(item, estoqueItem)),
+          map(() => item)
+        )
+      )
+    );
+  }
+
+  //Adicionando o Preço!
+  private adicionarPrecoItem(item: Item, itemPreco: ItemPreco[]): void {
+    item.precoPor = itemPreco[0].preco.precoPor;
+  }
+
+  //Adicionando o Estoque!
+  private adicionarEstoqueItem(item: Item, estoqueItem: EstoqueItem): void {
+    item.estoqueLoja = estoqueItem[0].estoqueLoja;
+  }
+
+
+  //Abre e Envia o Código do Item para Modal
+  openDialog(item: number): void {
+    const dialogRef = this.dialog.open(ModalComponent, {
+      width: '50%',
+      data: item
+    });
+  }
+
+}
